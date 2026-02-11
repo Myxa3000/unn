e47bc6b2b8c7cc45f73755b68f29e8a06c51ae6c
myxa <151192514+Myxa3000@users.noreply.github.com>
2026-02-11 10:39:32 +0300

    card 1 (11.02.2026)

---
+++ let me study.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+#include <complex>
+#include<fstream>
+#include<string>
+using namespace std;
+
+//
+//2
+//
+//int main()
+//{
+//	int n; int ans = 0;
+//	cout << "enter number of elemnts" << endl; cin >> n;
+//
+//	int* array = new int[n];
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << "enter " << i + 1 << " elemnet:" << endl;
+//		cin >> array[i];
+//	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (finder(array[i], array) > 1)
+//		{
+//			ans++;
+//		}
+//	}
+//	cout << "the answer is: " << ans << endl;
+//}
+//
+//int finder(int x, int*  ar)
+//{
+//	int res = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (x == ar[i])
+//		{
+//			res++;
+//		}
+//	}
+//	return res;
+//}
+
+//1
+
+int main()
+{
+	ifstream file("f1.txt");
+	if (file.is_open())
+	{
+
+	}
+}
